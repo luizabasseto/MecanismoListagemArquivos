a1df511532a51b99b1f6d22577c97c02db24f19f
luizabasseto <luizabasseto.1@gmail.com>
2025-11-22 10:42:56 -0300

    termino de comentarios

--- serializer/serializer.cpp
+++ serializer/serializer.cpp
@@ -7,16 +7,19 @@
 
 using namespace std;
 
+//le um numero int e retorna ele em um ponteiro de char(bytes) transformando para binário
 void writeInteger(std::ofstream& out, int valor) {
     out.write(reinterpret_cast<const char*>(&valor), sizeof(valor));
 }
 
+//le um numero em bytes (binario) e retorna o valor em int
 int readInteger(std::ifstream& in) {
     int valor;
     in.read(reinterpret_cast<char*>(&valor), sizeof(valor));
     return valor;
 }
 
+//escreve a string em binario
 void writeString(std::ofstream& out, const std::string& s) {
     int tamanho = s.size();
     writeInteger(out, tamanho);
@@ -25,6 +28,7 @@ void writeString(std::ofstream& out, const std::string& s) {
     }
 }
 
+//le a string em binario
 std::string readString(std::ifstream& in) {
     int tamanho = readInteger(in);
     if (tamanho == 0) return "";
@@ -36,6 +40,7 @@ std::string readString(std::ifstream& in) {
 }
 
 
+//serialize o Index e transforma em binario
 void Serializer::createArchiveBin(const Index& index, const std::string& caminhoArquivo) {
     std::ofstream out(caminhoArquivo, std::ios::binary);
     
@@ -44,30 +49,36 @@ void Serializer::createArchiveBin(const Index& index, const std::string& caminho
         return;
     }
 
+    //guarda o caminho do arquivo das stopWords e onde estão os arquivos base ( no caso library)
     writeString(out, index.diretorioBase); 
     writeString(out, index.arquivoStopWords); 
 
     writeInteger(out, index.nextId);
 
-    writeInteger(out, index.nextId);
-
+    //escreve primeiro a quantidade de arquivos gerais
     int nArquivo = index.mapIdToName.size();
     writeInteger(out, nArquivo);
 
+    //escreve par por par de ids e nomes dos arquivos
     for (const auto& par : index.mapIdToName) {
         writeInteger(out, par.first);
         writeString(out, par.second);
     }
 
+    //escreve a quantidade de palavras existentes em todos os arquivos
     int nPalavras = index.invertedIndex.size();
     writeInteger(out, nPalavras);
 
+    //escreve deopois par por par do index invertido (ids e palavras)
     for (const auto& par : index.invertedIndex) {
+        //escreve a palavra
         writeString(out, par.first);
         
         const std::set<int>& ids = par.second;
+        //escreve em quantos documentos a palavra esta
         writeInteger(out, ids.size());
         
+        //escreve os ids dos documentos onde a palavra esta
         for (int id : ids) {
             writeInteger(out, id);
         }
@@ -81,18 +92,19 @@ Index Serializer::ReadArchiveBin(const std::string& caminhoArquivo) {
     Index index;
     std::ifstream in(caminhoArquivo, std::ios::binary);
 
-    index.diretorioBase = readString(in);
-    index.arquivoStopWords = readString(in); 
-    index.nextId = readInteger(in);
-
     if (!in.is_open()) {
         std::cout << "Erro, arquivo de indice nao encontrado.\n";
         return index;
     }
 
+    //lê os diretorios da stopWords e do diretorio dos arquivos
+    index.diretorioBase = readString(in);
+    index.arquivoStopWords = readString(in); 
+
     index.nextId = readInteger(in);
     int nArquivo = readInteger(in);
 
+    //le os ids e nome dos arquivos
     for (int i = 0; i < nArquivo; ++i) {
         int id = readInteger(in);
         std::string nome = readString(in);
@@ -101,16 +113,20 @@ Index Serializer::ReadArchiveBin(const std::string& caminhoArquivo) {
         index.mapNameToId[nome] = id;
     }
 
+    //lê as palavras
     int nPalavras = readInteger(in);
     for (int i = 0; i < nPalavras; ++i) {
         std::string palavra = readString(in);
+        //lê em quantos ids (documentos) a palavra aparece
         int qtdIds = readInteger(in);
         
         std::set<int> ids;
         for (int j = 0; j < qtdIds; ++j) {
+            //lê os id e insere no set
             int id = readInteger(in);
             ids.insert(id);
         }
+        //associa cada palavra ao id dela
         index.invertedIndex[palavra] = ids;
     }
 
--- textProcessor/text.cpp
+++ textProcessor/text.cpp
@@ -9,11 +9,13 @@
 namespace fs = std::filesystem;
 
 TextProcessor::TextProcessor(const std::string& caminhoStopWords) {
+    //pega o caminho absoluto do arquivo de StopWords
     fs::path caminho = fs::absolute(caminhoStopWords);
     loadStopWords(caminho.string());
 }
 
 void TextProcessor::loadStopWords(const std::string& caminhoArquivo) {
+    //abre o arquivo das stop words
     std::ifstream arquivo(caminhoArquivo);
     if (!arquivo.is_open()) {
         std::cout << "não foi possivel abrir o arquivo de stop words: " << caminhoArquivo << std::endl;
@@ -21,7 +23,9 @@ void TextProcessor::loadStopWords(const std::string& caminhoArquivo) {
     }
 
     std::string palavra;
+    //faz passar por cada palavra do arquivo
     while (arquivo >> palavra) {
+        //a cada palavra, limpa ela (por garantia) e depois insere no unordered_set de stopWords
         stopWords.insert(clearWords(palavra));
     }
     arquivo.close();
@@ -29,31 +33,40 @@ void TextProcessor::loadStopWords(const std::string& caminhoArquivo) {
 
 std::string TextProcessor::clearWords(const std::string& palavra) {
     std::string limpa;
+    //reserve a qauntidade de espaço necessário para a palavra
     limpa.reserve(palavra.size());
 
     for (char c : palavra) {
+        //passa caractere por caractere da palavra, verifica se não é espaço e se não é pontuação
         if (!std::ispunct(static_cast<unsigned char>(c)) && 
             !std::isspace(static_cast<unsigned char>(c))) {
+                //se não é, transforma o caractere para minusculo
             limpa += std::tolower(static_cast<unsigned char>(c));
         }
     }
+    //retorna a palavra limpa
     return limpa;
 }
 
 std::vector<std::string> TextProcessor::processedTexts(const std::string& texto) {
     std::vector<std::string> palavrasFinais;
+    //transforma o texto em uma stringstream que quebra em "palavras" o texto inteiro 
     std::stringstream ss(texto);
     std::string palavra;
 
     while (ss >> palavra) {
+        //para cada palavra do ss, pega ela e limpa para tirar pontuações e deixar em minusculo
         std::string palavraLimpa = clearWords(palavra);
 
+        //se a palavra nao foi limpa por completo e nao sobrou nada
         if (palavraLimpa.empty()) continue;
 
+        //se a palavra limpa nao é uma das stop words
         if (stopWords.find(palavraLimpa) == stopWords.end()) {
+            //adiciona nas palavras finais
             palavrasFinais.push_back(palavraLimpa);
         }
     }
-
+    //tokenização, retorna uma lista de palavras do texto.
     return palavrasFinais;
 }
